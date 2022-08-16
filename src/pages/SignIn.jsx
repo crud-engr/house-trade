c11ee27b86fc299a1bbf4f7deb8afddc39afcf4e
Abeeb <abeebayinla@gmail.com>
2022-08-16 01:56:28 +0100

    added sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,81 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 
 const SignIn = () => {
-  return (
-    <h1>SignIn</h1>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-export default SignIn
+  // get data from form
+  const { email, password } = formData;
+
+  // initialize useNavigate hook
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+            <p
+              className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            >
+              {showPassword ? 'hide' : 'show'}
+            </p>
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Passwprd
+          </Link>
+
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        {/* Google Oauth */}
+
+        <Link to='/sign-up' className='registerLink'>
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
